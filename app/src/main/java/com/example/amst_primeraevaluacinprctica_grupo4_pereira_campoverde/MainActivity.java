ad4f41ef94a6b54933e48e16acf2380c4c75d071
robertvala <robavala@espol.edu.ec>
2020-11-27 14:00:14 -0500

    Creacion de preguntas

--- app/src/main/java/com/example/amst_primeraevaluacinprctica_grupo4_pereira_campoverde/MainActivity.java
+++ app/src/main/java/com/example/amst_primeraevaluacinprctica_grupo4_pereira_campoverde/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.amst_primeraevaluacinprctica_grupo4_pereira_campoverde;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import modelo.Pregunta;
@@ -12,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Pregunta pregunta1= new Pregunta();
+
+        Pregunta pregunta1= new Pregunta("Quien es este personaje de la saga de Harry Potter?","Ron","Hermione","Voldemort","Draco Malfoy","Draco Malfoy","DracoMalfoy");
+        Pregunta pregunta2= new Pregunta("Que personaje perdio una zapatilla","Blanca Nieves","La bella durmiente","Cenicienta","Rapunzel","Rapunzel","cenicienta");
+        Pregunta pregunta3= new Pregunta("Como se llama este personaje?","Chuwaka","Baby Yoda","Obi wan","Luke","Baby Yoda","babyyoda");
+        Pregunta pregunta4= new Pregunta("Como se llama la hermana de Luke SkyWalker?","Leia","Padme ","Jessica","Zola","Leia","leya");
+        Pregunta pregunta5= new Pregunta("Quien es la novia de la bestia?","Blanca Nieves","Bella","Malefica","Cenicienta","Bella","bella");
     }
 }
