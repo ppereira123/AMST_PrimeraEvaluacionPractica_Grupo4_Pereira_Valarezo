0e4a751a9e496c06a0cb62b58cf6d5c304c79cb0
robertvala <robavala@espol.edu.ec>
2020-11-27 15:02:05 -0500

    actualizacion del layout preguntas

--- app/src/main/java/com/example/amst_primeraevaluacinprctica_grupo4_pereira_campoverde/MainActivity.java
+++ app/src/main/java/com/example/amst_primeraevaluacinprctica_grupo4_pereira_campoverde/MainActivity.java
@@ -2,22 +2,36 @@ package com.example.amst_primeraevaluacinprctica_grupo4_pereira_campoverde;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.Parcelable;
 
 import modelo.Pregunta;
 
 public class MainActivity extends AppCompatActivity {
+    Pregunta pregunta1;
+    Pregunta pregunta2;
+    Pregunta pregunta3;
+    Pregunta pregunta4;
+    Pregunta pregunta5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Pregunta pregunta1= new Pregunta("Quien es este personaje de la saga de Harry Potter?","Ron","Hermione","Voldemort","Draco Malfoy","Draco Malfoy","DracoMalfoy");
+        Pregunta pregunta1= new Pregunta("Quien es este personaje de la saga de Harry Potter?","Ron","Hermione","Voldemort","Draco Malfoy","Draco Malfoy","dracomalfoy");
         Pregunta pregunta2= new Pregunta("Que personaje perdio una zapatilla","Blanca Nieves","La bella durmiente","Cenicienta","Rapunzel","Rapunzel","cenicienta");
         Pregunta pregunta3= new Pregunta("Como se llama este personaje?","Chuwaka","Baby Yoda","Obi wan","Luke","Baby Yoda","babyyoda");
         Pregunta pregunta4= new Pregunta("Como se llama la hermana de Luke SkyWalker?","Leia","Padme ","Jessica","Zola","Leia","leya");
         Pregunta pregunta5= new Pregunta("Quien es la novia de la bestia?","Blanca Nieves","Bella","Malefica","Cenicienta","Bella","bella");
+        Intent intent= new Intent(this,Preguntas.class);
+        intent.putExtra("pregunta1", (Parcelable) pregunta1);
+        intent.putExtra("pregunta2", (Parcelable) pregunta2);
+        intent.putExtra("pregunta3", (Parcelable) pregunta3);
+        intent.putExtra("pregunta4", (Parcelable) pregunta4);
+        intent.putExtra("pregunta5", (Parcelable) pregunta5);
+
     }
 }
--- app/src/main/java/com/example/amst_primeraevaluacinprctica_grupo4_pereira_campoverde/Preguntas.java
+++ app/src/main/java/com/example/amst_primeraevaluacinprctica_grupo4_pereira_campoverde/Preguntas.java
@@ -3,12 +3,26 @@ package com.example.amst_primeraevaluacinprctica_grupo4_pereira_campoverde;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 public class Preguntas extends AppCompatActivity {
+    ImageView imageView;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    TextView idPregunta;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preguntas);
+        imageView=(ImageView)findViewById(R.id.imagen);
+        button1=(Button)findViewById(R.id.button1);
     }
 }
